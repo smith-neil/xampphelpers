4915dcf9fe526cf07ee4ccd4bdf92990338c193c
Neil Smith <smith.neil.h@gmail.com>
2019-05-08 21:08:05 -0500

    initial commit

---
+++ make-wp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Setup Apache - Add VirtualHost and Update hosts
+
+
+# Setup Databse - Create db and user
+
+
+# Create site directory
+
+
+# Install WordPress
+
+
+# Setup WordPress config
+
+
+# Restart Apache
